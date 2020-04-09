f31c71d31d263373f5d5eaf588f76e51125b3e7d
Sobaken27 <48553021+Sobaken27@users.noreply.github.com>
2020-04-09 17:40:10 +0300

    Com

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ $(document).ready(function () {
 			scrollTop: top
 		}, 1000);
 	});
+      $('.menu-trigger').on('click', function() {
+       $('.menu').slideToggle(300, function(){
+            if( $(this).css('display') === "none"){
+                $(this).removeAttr('style');
+            }
+       });
+
+    });
+    
 });
