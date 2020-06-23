00d1208960887540dfe82711b16ccb322ee4ecfe
Sobaken27 <48553021+Sobaken27@users.noreply.github.com>
2020-06-23 11:02:02 +0300

    Commit

---
+++ ajax/mail.php
@@ -0,0 +1,17 @@
+<?php
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  $dsn = 'mysql:host=localhost;dbname=to-do';
+  $db = new PDO($dsn, 'root', 'root');
+
+  $stmt = $db->prepare("INSERT INTO request (name, email, subject, message) VALUES (:name, :email, :subject, :message)");
+  $stmt->bindValue(':name', $name);
+  $stmt->bindValue(':email', $email);
+  $stmt->bindValue(':subject', $subject);
+  $stmt->bindValue(':message', $message);
+  $stmt->execute();
+
+?>
--- script.js
+++ script.js
@@ -15,5 +15,39 @@ $(document).ready(function () {
        });
 
     });
+    $("#sendMail").on("click", function() {
+	let name = $("#name").val();
+	let email = $("#email").val();
+	let subject = $("#subject").val();
+	let message = $("#message").val();
+
+    if(name == "") {
+        alert("Введите ваше имя.");
+    	return false;
+    } else if (email == "") {
+    	alert("Введите ваш e-mail.");
+    	return false;
+    } else if (subject == "") {
+    	alert("Введите тему сообщения.");
+    	return false;
+    } else if (message == "") {
+    	alert("Введите сообщение.");
+    	return false;
+    } 
     
+$.ajax({
+url: '../ajax/mail.php',
+type: 'POST',
+cache: false,
+data: { 'name': name, 'email': email, 'subject': subject, 'message': message },
+dataType: 'html',
+beforeSend: function() {
+    $("sendMail").prop("disabled", true);
+},
+success: function() {
+    alert('Ваше сообщение отправлено');
+    $("#sendPoll").prop("disabled", false);
+}
+});
+});
 });
